658ad983279c0b70ca761df664e675891c9f147e
unknown <you@example.com>
2025-08-08 13:29:06 +0530

    Remove problematic test files causing UTF-8 encoding issues in Railway build

--- sitara777_app/android/app/src/main/kotlin/com/example/sitara777/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sitara777
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- sitara777_app/android/app/src/main/kotlin/com/sitara777/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sitara777.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
